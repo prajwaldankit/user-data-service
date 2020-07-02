af196ff0766e1a39bcccb2751e3ce4f49a587e18
Prajwal Dahal <prajwal.x82@gmail.com>
2020-07-02 21:02:43 +0545

    UDS1: Get All Users and Single User

---
+++ src/users/interfaces/user.interface.ts
@@ -0,0 +1,12 @@
+export interface User {
+  id: number;
+  name: string;
+  gender: string;
+  phone: number;
+  email: string;
+  address: string;
+  nationality: string;
+  dob: Date;
+  edBackground: string;
+  modeOfContact: number;
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@nestjs/common';
+
+import { User } from './interfaces/user.interface';
+import * as csvServices from './../common/csv';
+import { UserDto } from './dto/user.dto';
+
+@Injectable()
+export class UsersService {
+  private users: User[];
+  private filePath: string;
+  constructor() {
+    this.users = [];
+    this.populateUsers();
+  }
+
+  async populateUsers(): Promise<User[]> {
+    const usersFromCsv = await csvServices.populateUsers();
+    this.users = usersFromCsv;
+    return this.users;
+  }
+
+  async findAllUsers(): Promise<User[]> {
+    csvServices.populateUsers();
+    return this.users;
+  }
+
+  async createNewUser(data: UserDto): Promise<User> {
+    this.populateUsers();
+    const userIndex: number = this.users.length;
+
+    await csvServices
+      .addNewUserToCsv(userIndex, data)
+      .then(() => this.populateUsers());
+    return this.users[userIndex];
+  }
+
+  findOneUser(): User {
+    return this.users[0];
+  }
+}
